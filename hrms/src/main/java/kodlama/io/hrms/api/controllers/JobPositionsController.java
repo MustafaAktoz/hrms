a7a471cf695a1df84cf7703398f04b304e20de32
Ben Benim <Ben_Benim@outlook.com.tr>
2022-05-02 21:56:50 +0300

    .

--- hrms/src/main/java/kodlama/io/hrms/api/controllers/JobPositionsController.java
+++ hrms/src/main/java/kodlama/io/hrms/api/controllers/JobPositionsController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,7 +23,7 @@ public class JobPositionsController {
 		this.jobPositionService = jobPositionService;
 	}
 	
-	@GetMapping("add")
+	@PostMapping("add")
 	public ResponseEntity<?> add(@Valid @RequestBody JobPosition jobPosition){
 		var result = jobPositionService.add(jobPosition);
 		if(!result.isSuccess()) ResponseEntity.badRequest().body(result);
